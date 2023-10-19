a5bf566fe936837701d06283aa25c7a68cb3da02
Arn <arn.arn3328@gmail.com>
2023-10-19 16:02:02 +0700

    feat(app): get word api and optimized dfa code

--- routes/route.py
+++ routes/route.py
@@ -1,9 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from utils.words_dfa import isAccepted
+from database.words import wordlist
+import random
 
 router = APIRouter()
 
 
 @router.get("/word/check")
 async def check_word(word: str):
+    if not word.isalpha():
+        raise HTTPException(status_code=400, detail="Only alphabet character.");
     return (isAccepted(word.lower()))
+
+@router.get("/words")
+async def get_words(words: int = 5):
+    words = 22 if words > 22 else words
+    return random.sample(wordlist, words);
